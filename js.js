6888e63081816a5f44f9294a1015a6997564563a
Start Coding Student 1 <session1@podium3mac.west-mec.local>
2015-10-23 15:25:34 -0700

    first commit, friday oct 23 - Andy //added index.html, jQuery.js, stylesheet.css, and js.js

---
+++ js.js
@@ -0,0 +1,12 @@
+/**
+ * Created by session1 on 10/22/15.
+ */
+function addRow(){
+    $('#tb').append("<tr>" +
+        "<td></td>" +//food item
+        "<td></td>" +//description
+        "<td></td>" +//shelf life
+        "<td></td>" +//purchase date
+        "<td></td>" +//expiration date
+        "</tr>")
+}
